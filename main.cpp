58b55375e73fa4b9c4c74896da65c9db814cc8ec
Sergei Ziuzev <sergei.z@email.com>
2022-03-17 23:45:46 +0300

    check

--- main.cpp
+++ main.cpp
@@ -90,7 +90,7 @@ std::string ParseEvent(std::istream& is) {
 void TestAll() {
     TestRunner tr;
     tr.RunTest(TestParseEvent, "TestParseEvent");
-    tr.RunTest(TestParseCondition, "TestParseCondition");
+    // tr.RunTest(TestParseCondition, "TestParseCondition");
     // tr.RunTest(TestAdd, "Database test Add");
     // tr.RunTest(TestFind, "Database test Find");
     // tr.RunTest(TestLast, "Database test Last");
@@ -102,15 +102,15 @@ void TestParseEvent() {
         std::istringstream is("event");
         AssertEqual(ParseEvent(is), "event", "Parse event without leading spaces");
     }
-    // {
-    //     std::istringstream is("   sport event ");
-    //     AssertEqual(ParseEvent(is), "sport event ", "Parse event with leading spaces");
-    // }
-    // {
-    //     std::istringstream is("  first event  \n  second event");
-    //     std::vector<std::string> events;
-    //     events.push_back(ParseEvent(is));
-    //     events.push_back(ParseEvent(is));
-    //     AssertEqual(events, std::vector<std::string>{"first event  ", "second event"}, "Parse multiple events");
-    // }
+    {
+        std::istringstream is("   sport event ");
+        AssertEqual(ParseEvent(is), "sport event ", "Parse event with leading spaces");
+    }
+    {
+        std::istringstream is("  first event  \n  second event");
+        std::vector<std::string> events;
+        events.push_back(ParseEvent(is));
+        events.push_back(ParseEvent(is));
+        AssertEqual(events, std::vector<std::string>{"first event  ", "second event"}, "Parse multiple events");
+    }
 }
